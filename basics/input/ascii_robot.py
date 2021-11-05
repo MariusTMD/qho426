23d72421ffdfae643481f44e0e3c0b2815f2221d
Marius Dănuț <92996215+MariusTMD@users.noreply.github.com>
2021-11-05 16:15:07 +0000

    Added code to display a simple message.

--- basics/input/ascii_robot.py
+++
@@ -1,9 +0,0 @@
-# Ask user for eye character 
-print("Please enter eye character")
-eye = input() 
-
-# Display an ascii art robot
-print("##########")
-print(f"#  {eye}  {eye}   #")
-print("#  ----  #")
-print("##########")
